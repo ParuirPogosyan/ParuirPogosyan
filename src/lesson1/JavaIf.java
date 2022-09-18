dda688a842b0182006dddd26f99a77e4c05af2f0
vazgengalstyan <psyuzanna13@gmail.com>
2022-09-18 19:13:07 +0400

    lesson12

---
+++ src/lesson1/JavaIf.java
@@ -0,0 +1,14 @@
+package lesson1;
+
+public class JavaIf {
+    public static void main(String[] args) {
+        int num=120;
+        if (num>30){//Ճիշտ տարբերակ
+            System.out.println("Թիվը մեծ է 30-ից");
+
+            if (num<30){//Սխալ տարբերակ
+                System.out.println("Թիվը մեծ է 30-ից");
+            }
+        }
+    }
+}

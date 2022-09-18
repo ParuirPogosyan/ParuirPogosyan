4ccf48d861a8ca36cbef289b8493ff4412dd9e03
vazgengalstyan <psyuzanna13@gmail.com>
2022-09-18 19:14:11 +0400

    test1

---
+++ src/lesson1/JavaFor.java
@@ -0,0 +1,11 @@
+package lesson1;
+
+public class JavaFor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i);
+
+        }
+
+    }
+}
